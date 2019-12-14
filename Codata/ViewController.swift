69b52c634c84348b10beecdf781f1d5dfa739d17
Darrell10 <frederick.port@me.com>
2019-12-14 17:17:36 +0100

    Initial Commit

---
+++ Codata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Codata
+//
+//  Created by Frederick Port on 14/12/2019.
+//  Copyright © 2019 Frederick Port. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
